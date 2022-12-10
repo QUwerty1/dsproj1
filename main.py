790396774cf1f214d69f6144e5ba29ee8c37f0ab
mrquw <mrquwerty@outlook.com>
2022-12-10 11:50:21 +0800

    com

--- main.py
+++ main.py
@@ -15,10 +15,15 @@ bot = commands.Bot(command_prefix=Settings.prefix, intents=intents)
 
 vc = ''
 
+
+@bot.command()
+async def discon(ctx):
+    await vc.disconnect()
+
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
 
 
 @bot.command()
@@ -33,9 +38,9 @@ async def pause(ctx):
 
 @bot.command()
 async def music(ctx, *, arg):
-    if not('vc' in locals()):
-        global vc
-        vc = await connect(ctx)
+    global vc
+    if not(type(vc) == discord.VoiceClient):
+        await connect(ctx)
     print(locals())
     if 'pause' in arg:
         vc.pause()
@@ -45,31 +50,31 @@ async def music(ctx, *, arg):
                 info = ydl.extract_info(arg, download=False)
             else:
                 info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
-
         url = info['formats'][0]['url']
         vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS))
 
 
 @bot.command()
 async def connect(ctx):
+    global vc
     vc = await ctx.message.author.voice.channel.connect()
-    return vc
 
 
-# @bot.event
-# async def on_ready():
-#     print('We have been logged in as {0.user}'.format(bot))
-#
-#
-# @bot.event
-# async def on_message(message):
-#     with open('logs/' + message.channel.name + '.txt', mode='a') as f:
-#         f.write(f'{message.author.name}: {message.content} \n')
-#     print(f'{message.author.name}: {message.content}')
-#     if message.author == bot.user:
-#         return
-#     if message.content.startswith('Botyra'):
-#         await message.channel.send(f'Hi! {message.author.mention}')
+@bot.event
+async def on_ready():
+    print('We have been logged in as {0.user}'.format(bot))
+
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    with open('logs/' + message.channel.name + '.txt', mode='a') as f:
+        f.write(f'{message.author.name}: {message.content} \n')
+    print(f'{message.author.name}: {message.content}')
+    if message.author == bot.user:
+        return
+    if message.content.startswith('Botyra'):
+        await message.channel.send(f'Hi! {message.author.mention}')
 
 bot.run(Settings.token)
 
